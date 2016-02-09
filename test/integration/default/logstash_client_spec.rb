d6203f604abd20384e0b1416d7768e446a4be0aa
Conor Schaefer <conor.schaefer@gmail.com>
2016-02-08 19:07:38 -0800

    More inspec tests

--- test/integration/default/logstash_client_spec.rb
+++ test/integration/default/logstash_client_spec.rb
@@ -1,4 +1,22 @@
-describe package("filebeat") do
+describe package('filebeat') do
   it { should be_installed }
-  its("version") { should eq "1.1.0" }
+  its('version') { should eq '1.1.0' }
+end
+
+describe package('topbeat') do
+  it { should be_installed }
+  its('version') { should eq '1.1.0' }
+end
+
+describe apt('http://packages.elasticsearch.org/logstashforwarder/debian') do
+  it { should_not exist }
+  it { should_not be_enabled }
+end
+describe package('logstash-forwarder') do
+  it { should_not be_installed }
+end
+
+describe apt('http://packages.elastic.co/beats/apt') do
+  it { should exist }
+  it { should be_enabled }
 end
