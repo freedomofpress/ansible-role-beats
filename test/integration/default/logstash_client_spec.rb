9b11ef3f367c338f4c5526e4f7b750fdd5294e88
Conor Schaefer <conor.schaefer@gmail.com>
2016-02-08 19:23:06 -0800

    Adds inspec tests for apt config and service checks

--- test/integration/default/logstash_client_spec.rb
+++ test/integration/default/logstash_client_spec.rb
@@ -20,3 +20,30 @@ describe apt('http://packages.elastic.co/beats/apt') do
   it { should exist }
   it { should be_enabled }
 end
+
+
+describe command('apt-key adv --fingerprint --list-keys --keyring /etc/apt/trusted.gpg 46095ACC8548582C1A2699A9D27D666CD88E42B4') do
+  its('exit_status') { should eq 0 }
+  elasticsearch_gpg_fingerprint = <<FINGERPRINT
+pub   2048R/D88E42B4 2013-09-16
+      Key fingerprint = 4609 5ACC 8548 582C 1A26  99A9 D27D 666C D88E 42B4
+uid                  Elasticsearch (Elasticsearch Signing Key) <dev_ops@elasticsearch.org>
+sub   2048R/60D31954 2013-09-16
+FINGERPRINT
+  its('stdout') { should include elasticsearch_gpg_fingerprint }
+end
+
+
+describe file('/etc/topbeat/topbeat.yml') do
+  it { should be_owned_by('root') }
+  it { should be_grouped_into('root') }
+  its('mode') { should eq 0644 }
+end
+
+describe service('filebeat') do
+  it { should be_running }
+end
+
+describe service('topbeat') do
+  it { should be_running }
+end
