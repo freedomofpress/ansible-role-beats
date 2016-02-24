8de7cc9c34cef4a9599663bf42b5fae4016523fe
Conor Schaefer <conor.schaefer@gmail.com>
2016-02-23 17:22:39 -0800

    Updates all serverspec tests post conversion

--- spec/logstash_client_spec.rb
+++ spec/logstash_client_spec.rb
@@ -1,28 +1,30 @@
+require 'spec_helper'
+
 describe package('filebeat') do
   it { should be_installed }
-  its('version') { should eq '1.1.0' }
+  its('version') { should >= '1.1.0' }
 end
 
 describe package('topbeat') do
   it { should be_installed }
-  its('version') { should eq '1.1.0' }
+  its('version') { should >= '1.1.0' }
 end
 
-describe apt('http://packages.elasticsearch.org/logstashforwarder/debian') do
-  it { should_not exist }
-  it { should_not be_enabled }
-end
 describe package('logstash-forwarder') do
   it { should_not be_installed }
 end
 
-describe apt('http://packages.elastic.co/beats/apt') do
-  it { should exist }
-  it { should be_enabled }
+describe command('apt-cache policy') do
+  its('stdout') do
+    should include '500 http://packages.elastic.co/beats/apt/ '\
+                   'stable/main amd64'
+    should_not include 'http://packages.elasticsearch.org/logstashforwarder/debian'
+  end
 end
 
-
-describe command('apt-key adv --fingerprint --list-keys --keyring /etc/apt/trusted.gpg 46095ACC8548582C1A2699A9D27D666CD88E42B4') do
+describe command('apt-key adv --fingerprint --list-keys \
+                 --keyring /etc/apt/trusted.gpg \
+                 46095ACC8548582C1A2699A9D27D666CD88E42B4') do
   its('exit_status') { should eq 0 }
   elasticsearch_gpg_fingerprint = <<FINGERPRINT
 pub   2048R/D88E42B4 2013-09-16
@@ -33,11 +35,10 @@ FINGERPRINT
   its('stdout') { should include elasticsearch_gpg_fingerprint }
 end
 
-
 describe file('/etc/topbeat/topbeat.yml') do
   it { should be_owned_by('root') }
   it { should be_grouped_into('root') }
-  its('mode') { should eq 0644 }
+  its('mode') { should eq '644' }
 end
 
 describe command('topbeat -configtest -c /etc/topbeat/topbeat.yml') do
